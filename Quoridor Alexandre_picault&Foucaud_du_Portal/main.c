8349c4569d2c475371e7936f12c2859bc2aa882e
Foucaud du Portal <fcduportal@gmail.com>
2020-03-15 09:47:52 +0100

    Initial Commit

---
+++ Quoridor Alexandre_picault&Foucaud_du_Portal/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Quoridor Alexandre_picault&Foucaud_du_Portal
+//
+//  Created by Foucaud de Bouët du Portal on 15/03/2020.
+//  Copyright © 2020 Foucaud de Bouët du Portal&Alexandre Picault. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
