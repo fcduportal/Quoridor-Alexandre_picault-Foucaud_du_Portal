9378a3fc7d230ffd2f26c5e6c00ce339a8afcee3
Alexandre <alexandre.picault@gmail.com>
2020-04-27 09:14:44 +0200

    jouer

---
+++ Quoridor Alexandre_picault&Foucaud_du_Portal/partie_jouer.c
@@ -0,0 +1,29 @@
+//
+//  partie_jouer.c
+//  Quoridor Alexandre_picault&Foucaud_du_Portal
+//
+//  Created by Alexandre Picault on 25/04/2020.
+//  Copyright © 2020 Foucaud de Bouët du Portal&Alexandre Picault. All rights reserved.
+//
+
+#include "partie_jouer.h"
+int choice=0;
+{
+    printf("si vous voulez jouer une barriere taper 1 ou un pion taper 2:/n");
+    scanf("%d",&choice);
+    if (choice==1)
+    {
+        //struct combien il rete de barriere
+        if (fence>0)
+        {
+            //placement sur le plateau ?
+            
+        }
+        
+    }
+    if (choice==2)
+    {
+        
+    }
+    
+}
